46ad42b1e368949e9e575de6111b6a8d9a30fa87
rahul singh <rahul2004ruby@gmail.com>
2025-02-26 23:53:30 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite's default port
+    allow_origins=["https://backendfor-rock-sense.vercel.app/"],  # Vite's default port
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
